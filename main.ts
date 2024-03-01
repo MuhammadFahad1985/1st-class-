664b903ece0eedc704fbbd2842ab911f7f66a3d3
MuhammadFahad1985 <161850246+MuhammadFahad1985@users.noreply.github.com>
2024-03-01 19:45:22 +0500

    first file of hellow world

---
+++ main.ts
@@ -0,0 +1,39 @@
+console.log("hello, world!"); 
+console.log("gerrys net");
+console.log(123)
+let variable : string = "hello world"
+console.log(variable)
+let sirfaisal : string ="Teacher"
+let sirimran : string ="Teacher"
+let sirfaisal1 : string ="Teacher"
+let sir_imran : string ="Teacher"
+let $sir_imran: string ="Teacher"
+let 2sir_imran : string ="Teacher"
+let Sir2imran : string ="Teacher"
+let $_sir_imransirimran$ : string ="Teacher"
+let $ : string ="Teacher"
+let $2 : string ="Teacher2"
+let a: string ="Teacher"
+let variableA : string ="some string"
+// strongly types variable
+/* Another Comment
+something
+*/
+variableA =2:
+let num: number=1
+num="any string";
+variableA = "any string";
+//Type inference 
+let variableB ="some string";
+variableB= "2";
+
+let varC;
+
+const variableD ="here is const";
+variableD = "Reassigning"
+
+lett variable: string = "something";
+console.log(variable);
+
+
+//consol.log(variable)
